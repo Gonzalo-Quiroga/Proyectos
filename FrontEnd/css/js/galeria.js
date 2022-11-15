4874ed50a60098af25fe851529b2a3afe6d05912
Gonzalo-Quiroga <gonzaloquiroga488@gmail.com>
2022-11-15 17:18:14 -0300

    Responsive 90%

---
+++ FrontEnd/css/js/galeria.js
@@ -0,0 +1,20 @@
+const images = document.querySelectorAll(".img");
+const containerImage = document.querySelector(".containerImg");
+const imageContainer = document.querySelector(".img-show");
+
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    addImage(image.getAttribute("src"), image.getAttribute("alt"));
+  });
+});
+
+const addImage = (srcImage, altImage) => {
+  containerImage.classList.toggle("move");
+  imageContainer.classList.toggle("show");
+  imageContainer.src = srcImage;
+};
+
+containerImage.addEventListener("click", () => {
+  containerImage.classList.toggle("move");
+  imageContainer.classList.toggle("show");
+});
